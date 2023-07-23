76e464e76614bf5d0194f92063f55036249b628e
Huihui Fang <fanghuihuibit@163.com>
2023-07-23 14:20:36 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,3 @@
+OMIA-X: 10th MICCAI Workshop on Ophthalmic Medical Image Analysis
+OMIA-X: 10th MICCAI Workshop on Ophthalmic Medical Image Analysis
+OMIA-X: 10th MICCAI Workshop on Ophthalmic Medical Image Analysis
